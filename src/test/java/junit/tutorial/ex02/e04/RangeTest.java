fbfe988253b76ac597f547a9aba5eb7d7b3f0631
SyunjiSasaki <shunji.sasaki@rakus-partners.co.jp>
2022-02-14 12:52:25 +0900

    RangeTest,parmeterized使用

--- src/test/java/junit/tutorial/ex02/e04/RangeTest.java
+++ src/test/java/junit/tutorial/ex02/e04/RangeTest.java
@@ -6,38 +6,52 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RangeTest {
 
 	Range range;
-
-	@DisplayName("min=0.0,max=10.5のRangeの時の各テスト")
+	
+	@DisplayName("min=0.0,max=10.5のRangeの時のTrueテスト")
 	@ParameterizedTest
-	public void test1() {
+	@ValueSource(doubles= {0.0,10.5})
+	public void test1(double n) {
 		range = new Range(0.0, 10.5);
 
-		assertFalse(range.contains(-0.1));
+		assertTrue(range.contains(n));
 
-		assertTrue(range.contains(0.0));
+	}
 
-		assertTrue(range.contains(10.5));
+	@DisplayName("min=0.0,max=10.5のRangeの時のFalseテスト")
+	@ParameterizedTest
+	@ValueSource(doubles= {-0.1,10.6})
+	public void test2(double n) {
+		range = new Range(0.0, 10.5);
 
-		assertFalse(range.contains(10.6));
+		assertFalse(range.contains(n));
 
 	}
 	
-	@DisplayName("min=-5.1,max=5.1のRangeの時の各テスト")
-	@Test
-	public void test2() {
+	
+	
+	@DisplayName("min=-5.1,max=5.1のRangeの時のTrueテスト")
+	@ParameterizedTest
+	@ValueSource(doubles= {-5.1,5.1})
+	public void test3(double n) {
 		range = new Range(-5.1, 5.1);
 
-		assertFalse(range.contains(-5.2));
+		assertTrue(range.contains(n));
 
-		assertTrue(range.contains(-5.1));
+	}
 
-		assertTrue(range.contains(5.1));
 
-		assertFalse(range.contains(5.2));
+	@DisplayName("min=-5.1,max=5.1のRangeの時のFalseテスト")
+	@ParameterizedTest
+	@ValueSource(doubles= {-5.2,5.2})
+	public void test4(double n) {
+		range = new Range(-5.1, 5.1);
+
+		assertFalse(range.contains(n));
 
 	}
 
