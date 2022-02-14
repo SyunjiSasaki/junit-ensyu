966c42cfbddc941685b63c21cb1e27658ea3a926
SyunjiSasaki <shunji.sasaki@rakus-partners.co.jp>
2022-02-14 10:10:45 +0900

    StringUtilsTest

--- src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -1,6 +1,6 @@
 package junit.tutorial.ex01.e01;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +14,20 @@ class StringUtilsTest {
 	
 	@Test
 	void test1() {
-		String result1;
+		String result1 = stringUtils.toSnakeCase("aaa");
+		assertEquals("aaa",result1,"aaa→aaa");
 	}
 	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test2() {
+		String result2 = stringUtils.toSnakeCase("HelloWorld");
+		assertEquals("hello_world",result2,"HelloWorld→hello_world");
 	}
-
+	
+	@Test
+	void test3() {
+		String result3 = stringUtils.toSnakeCase("practiceJunit");
+		assertEquals("practice_junit",result3,"practiceJunit→practice_junit");
+	}
+	
 }
